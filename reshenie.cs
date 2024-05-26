5a72f2327543738b31e448c92f5f83c6a32f94bc
Roman Kurakin <kurakinroman1606@gmail.com>
2024-05-26 23:59:18 +0300

    fix

--- reshenie.cs
+++ reshenie.cs
@@ -4,12 +4,10 @@ class Program
 {
     static void Main()
     {
-        // Вводим массив строк с клавиатуры или задаем его в коде
         Console.WriteLine("Введите строки через пробел:");
         string input = Console.ReadLine();
         string[] initialArray = input.Split(' ');
 
-        // Определяем размер нового массива
         int count = 0;
         foreach (string str in initialArray)
         {
@@ -19,7 +17,6 @@ class Program
             }
         }
 
-        // Создаем новый массив и заполняем его подходящими строками
         string[] newArray = new string[count];
         int index = 0;
         foreach (string str in initialArray)
@@ -31,7 +28,6 @@ class Program
             }
         }
 
-        // Выводим новый массив
         Console.WriteLine("Новый массив строк, длина которых меньше либо равна 3 символам:");
         foreach (string str in newArray)
         {
